adb877a2f5790ca4d7a7c1b03c8f1e2120b144c1
Pawar, Deepak Atmaram <deepawar@deloitte.com>
2023-03-12 16:18:44 +0530

    initial commit alog with code for backend

---
+++ server.js
@@ -0,0 +1,45 @@
+import express  from 'express'
+import mongoose from 'mongoose'
+import Cors from 'cors'
+import Cards from './dbCards.js'
+
+//App config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = "mongodb+srv://dpk9111996:tAZktFWZPIAnUbEG@cluster0.bkecu3w.mongodb.net/?retryWrites=true&w=majority"
+
+//Middleware
+app.use(express.json())
+app.use(Cors())
+
+//DB Config
+mongoose.connect(connection_url,{})
+
+//API Endpoints
+app.get("/", (req,res)=> res.status(200).send("Hello Deepawar"))
+app.post("/dating/cards",async (req,res)=>{
+console.log(req.body , req.params,"In request");
+const dbCard = req.body
+
+ const data = await Cards.create(dbCard)
+console.log(data);
+res.send(data);
+})
+
+app.get("/dating/cards",(req,res)=>{
+Cards.find((err,data)=>{
+
+if(err){
+res.status(500).send(err)
+}
+else{ 
+res.status(200).send(data)
+}
+})
+})
+
+
+
+//Listener
+
+app.listen(port,()=> console.log("he bholya shankara"));
